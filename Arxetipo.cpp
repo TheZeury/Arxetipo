32c60fbfe72e2a98d961948369fff936cdbe2527
TheZeury <cjrsacred@gmail.com>
2023-04-12 15:33:03 +0900

    OpenXR-SDK

---
+++ Arxetipo.cpp
@@ -0,0 +1,12 @@
+﻿// Arxetipo.cpp : Defines the entry point for the application.
+//
+
+#include "Arxetipo.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
